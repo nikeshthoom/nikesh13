74c134f9c750867f58cf5a665195723cd19f7016
nikeshthoom <nikeshnike997@gmail.com>
2026-02-06 13:15:38 +0530

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/le32")
+.then(()=>{
+    console.log("Mongo db connected")
+})
+.catch(()=>{
+    console.log("failed")
+})
+
+const LoginSchema=new mongoose.Schema({
+    name:String,
+    password:String,
+})
+
+const collection=new mongoose.model("le32",LoginSchema)
+module.exports=collection
+
+
